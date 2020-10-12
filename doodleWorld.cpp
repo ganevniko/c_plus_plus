61e5e2e5653e7778ec18aee41f86bb2b9bace920
Niko Ganev <40749238+ganevniko@users.noreply.github.com>
2020-10-12 14:44:38 -0400

    Update doodleWorld.cpp

--- doodleWorld.cpp
+++ doodleWorld.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 const int worldSize =5;
-const int initialNumberOfAnts = 2;
-const int initialNumberOfDoodleBugs = 1;
-const char antsAppearance = 'O';
+const int initialNumberOfAnts = 4;
+const int initialNumberOfDoodleBugs = 2;
+const char antsAppearance = 'o';
 const char doodleBugsAppearance = 'X';
 const char emptySpaceAppearance = '-';
-
+const char directions[4]={'l','r','u','d'};
 char doodleWorld[worldSize][worldSize];
 
 
@@ -19,6 +19,8 @@ public:
     organism(int row, int column);
     int getRow();
     int getColumn();
+    void setRow(int a);
+    void setColumn(int a);
     bool lifeCheck();
     void kill();
     char getLeft();
@@ -29,6 +31,11 @@ public:
     void setRight(char r);
     void setUp(char u);
     void setDown(char d);
+    void updateLeft();
+    void updateRight();
+    void updateUp();
+    void updateDown();
+    void lookAround();
     //virtual void move() const;
 private:
     int positionRow;
@@ -68,11 +75,20 @@ organism :: organism(int row, int column){
 }
 
 int organism :: getRow() {
-    return positionRow;
+    return this->positionRow;
 }
 int organism :: getColumn() {
     return positionColumn;
 }
+
+void organism::setRow(int a){
+    this->positionRow=a;
+}
+
+void organism::setColumn(int a){
+    this->positionColumn=a;
+}
+
 
 bool organism :: lifeCheck() {
     return isAlive;
@@ -97,22 +113,69 @@ char organism:: getDown(){
 }
 
 void organism:: setLeft(char l){
-     left =l;
+    this->left =l;
 }
 
 void organism:: setRight(char r){
-    right = r;
+    this->right = r;
 }
 void organism:: setUp(char u){
-    up = u;
+    this->up = u;
 }
 void organism:: setDown(char d){
-    down =d;
+    this->down =d;
 }
+
+void organism::updateUp(){
+    if (positionRow == 0)
+        up = '9';
+    else
+        up = doodleWorld[positionRow-1][positionColumn];
+
+
+}
+void organism::updateDown(){
+    if (positionRow == worldSize-1 )
+        down = '9';
+    else
+        down = doodleWorld[positionRow+1][positionColumn];
+
+
+}
+void organism::updateLeft(){
+    if (positionColumn == 0 )
+        left = '9';
+    else
+        left = doodleWorld[positionRow][positionColumn-1];
+
+}
+void organism::updateRight(){
+    if (positionColumn == worldSize-1 ) {
+        right = '9';
+    }
+
+    else {
+        right = doodleWorld[positionRow][positionColumn + 1];
+    }
+
+
+}
+
+void organism::lookAround(){
+    updateDown();
+    updateLeft();
+    updateRight();
+    updateUp();
+}
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////
 
 class ant : public organism {
 public:
     ant(int row, int column);
+    void breed();
+    void move();
+
 private:
     int stepsAlive;
     char appearance;
@@ -121,11 +184,64 @@ ant::ant(int row, int column):organism(row,column){
     appearance = antsAppearance ;
     stepsAlive =0;
 }
+void ant:: move(){
+    srand(time(0));
+    vector<char> whatsAvailable;
+    char decision='n';
+    int randomizing;
+    this->lookAround();
+    if (doodleWorld[getRow()][getColumn()]==doodleBugsAppearance)
+        kill();
+    else{
+        if (getRight() == emptySpaceAppearance)
+            whatsAvailable.push_back('r');
+        if (getLeft() == emptySpaceAppearance)
+            whatsAvailable.push_back('l');
+        if (getUp() == emptySpaceAppearance)
+            whatsAvailable.push_back('u');
+        if (getDown() == emptySpaceAppearance)
+            whatsAvailable.push_back('d');
+
+        if (!(whatsAvailable.empty())) {
+            randomizing = int(rand() % whatsAvailable.size());
+            decision = whatsAvailable[randomizing];
+        }
+    }
+        if (decision== 'r' || decision== 'd' || decision== 'u' || decision== 'l' ){
+            doodleWorld[getRow()][getColumn()] = emptySpaceAppearance;
+            if (decision == 'u' && doodleWorld[getRow()-1][getColumn()]!=antsAppearance) {
+                this->setRow(getRow() - 1);
+                doodleWorld[getRow()][getColumn()] = antsAppearance;
+            }
+            if (decision == 'd' && doodleWorld[getRow()+1][getColumn()]!=antsAppearance) {
+                this->setRow(getRow() + 1);
+                doodleWorld[getRow()][getColumn()] = antsAppearance;
+            }
+            if (decision == 'l' && doodleWorld[getRow()][getColumn()-1]!=antsAppearance) {
+                this->setColumn(getColumn() - 1);
+                doodleWorld[getRow()][getColumn()] = antsAppearance;
+            }
+            if (decision == 'r' && doodleWorld[getRow()][getColumn()+1]!=antsAppearance) {
+                this->setColumn(getColumn() + 1);
+                doodleWorld[getRow()][getColumn()] = antsAppearance;
+            }
+        }
+
+
+
+}
+void ant::breed(){
+
+
+}
+// ////////////////////////////////////////////////////////////////////////////
 
 
 class doodleBug : public organism{
 public:
     doodleBug(int row, int column);
+    void breed();
+    void move();
 private:
     int stepsAlive;
     char appearance;
@@ -135,14 +251,75 @@ doodleBug::doodleBug(int row, int column):organism(row,column){
     appearance = doodleBugsAppearance;
     stepsAlive =0;
 }
+void doodleBug:: breed(){
+
+}
+void doodleBug::move(){
+    srand(time(0));
+    vector<char> whatsAvailable;
+    char decision='n';
+    int randomizing;
+    this->lookAround();
+    if (getRight()==antsAppearance) {
+        decision = 'r';
+    }
+    else if (getLeft()==antsAppearance) {
+        decision = 'l';
+    }
+    else if (getUp()==antsAppearance) {
+        decision = 'u';
+    }
+    else if (getDown()==antsAppearance) {
+        decision = 'd';
+    }
+
+    if (decision!= 'r' && decision!= 'd' && decision!= 'u' && decision!= 'l' ) {
+        if (getRight() == emptySpaceAppearance)
+            whatsAvailable.push_back('r');
+        if (getLeft() == emptySpaceAppearance)
+            whatsAvailable.push_back('l');
+        if (getUp() == emptySpaceAppearance)
+            whatsAvailable.push_back('u');
+        if (getDown() == emptySpaceAppearance)
+            whatsAvailable.push_back('d');
+
+        if (!(whatsAvailable.empty())) {
+            randomizing = int(rand() % whatsAvailable.size());
+            decision = whatsAvailable[randomizing];
+        }
+    }
+
+    if (decision== 'r' || decision== 'd' || decision== 'u' || decision== 'l' ){
+        doodleWorld[getRow()][getColumn()] = emptySpaceAppearance;
+        if (decision == 'u' && doodleWorld[getRow()-1][getColumn()]!=doodleBugsAppearance) {
+            this->setRow(getRow() - 1);
+            doodleWorld[getRow()][getColumn()] = doodleBugsAppearance;
+        }
+        if (decision == 'd' && doodleWorld[getRow()+1][getColumn()]!=doodleBugsAppearance) {
+            this->setRow(getRow() + 1);
+            doodleWorld[getRow()][getColumn()] = doodleBugsAppearance;
+        }
+        if (decision == 'l' && doodleWorld[getRow()][getColumn()-1]!=doodleBugsAppearance) {
+            this->setColumn(getColumn() - 1);
+            doodleWorld[getRow()][getColumn()] = doodleBugsAppearance;
+        }
+        if (decision == 'r' && doodleWorld[getRow()][getColumn()+1]!=doodleBugsAppearance) {
+            this->setColumn(getColumn() + 1);
+            doodleWorld[getRow()][getColumn()] = doodleBugsAppearance;
+        }
+    }
+
+}
 
 void displayWorld(char arr[worldSize][worldSize]);
+
 int main(){
     srand(time(0));
     char next='y';
-    int tempRow=0, tempColumn=0;
+    int tempRow=0, tempColumn=0, selected_direction;
     vector<ant> antsArray;
     vector<doodleBug> doodleBugsArray;
+
 
     //Initializing world
     for (int i = 0; i < worldSize; ++i ) {
@@ -152,8 +329,8 @@ int main(){
     //Initializing ants
     for (int i=0; i<initialNumberOfAnts; i++){
         while (doodleWorld[tempRow][tempColumn]!=emptySpaceAppearance) {
-            tempRow = int(rand() % worldSize);
-            tempColumn = int(rand() % worldSize);
+            tempRow = int(rand() % worldSize)+1;
+            tempColumn = int(rand() % worldSize)+1;
         }
         antsArray.push_back(ant(tempRow, tempColumn));
         doodleWorld[tempRow][tempColumn]= antsAppearance;
@@ -169,18 +346,27 @@ int main(){
         doodleWorld[tempRow][tempColumn]= doodleBugsAppearance;
 
     }
+    for (int i=0; i<antsArray.size();i++)
+        antsArray[i].lookAround();
 
-
-
-    cout<<"The Current state of Doddle World is:"<<endl;
+    cout<<"The Current state of Doodle World is:"<<endl;
     displayWorld(doodleWorld);
+    cout<<" Are you ready to take the first step? if yes enter 'y'."<<endl;
+    cin>>next;
     while (next == 'y') {
-        cout << "To move one step further press \"Enter\" to end game enter n:" << endl;
-        next = cin.get();
-        if (next!='n') {
-            next = 'y';
-            displayWorld(doodleWorld);
+        for (int i=0; i<doodleBugsArray.size();i++) {
+            doodleBugsArray[i].move();
+            doodleBugsArray[i].lookAround();
         }
+        for (int i=0; i<antsArray.size();i++) {
+            antsArray[i].move();
+            antsArray[i].lookAround();
+        }
+
+        displayWorld(doodleWorld);
+        cout << "To move one step further press y" << endl;
+        cin>>next;
+
     }
 
     cout<<"right --> "<<antsArray[1].getRight()<<endl;
@@ -203,27 +389,6 @@ void displayWorld(char arr[worldSize][worldSize]){
 
 }
 
-void lookAround(organism org){
-    char l, r, u, d;
 
-    if (org.getRow() == 0)
-        org.setUp('9');
-    else
-        org.setUp(doodleWorld[org.getRow()-1][org.getColumn()]);
-    if (org.getRow() == worldSize-1 )
-        org.setDown('9');
-    else
-        org.setDown(doodleWorld[org.getRow()+1][org.getColumn()]);
-    if (org.getColumn() == 0 )
-        org.setLeft('9');
-    else
-        org.setLeft(doodleWorld[org.getRow()][org.getColumn()-1]);
-    if (org.getColumn() == worldSize-1 ) {
-        org.setRight('9');
-    }
 
-    else {
-        org.setRight(doodleWorld[org.getRow()][org.getColumn() + 1]);
-    }
 
-}
