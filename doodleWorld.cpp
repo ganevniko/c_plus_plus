b26678d506c0ea1fc0f3cf9c3cfbf24df43c058a
Niko Ganev <40749238+ganevniko@users.noreply.github.com>
2020-10-13 11:11:34 -0400

    Update doodleWorld.cpp

--- doodleWorld.cpp
+++ doodleWorld.cpp
@@ -4,14 +4,15 @@
 #include <vector>
 
 using namespace std;
-    const int worldSize =5;
-const int initialNumberOfAnts = 2;
+const int worldSize =5;
+const int initialNumberOfAnts = 0;
 const int initialNumberOfDoodleBugs = 1;
 const char antsAppearance = 'o';
 const char doodleBugsAppearance = 'X';
 const char emptySpaceAppearance = '-';
-const int doodleBugStepsToStarvation = 3;
+const int doodleBugStepsToStarvation = 10;
 const int antStepsToBreed =3;
+const int bugStepsToBreed =8;
 char doodleWorld[worldSize][worldSize];
 
 
@@ -33,6 +34,8 @@ public:
     void updateUp();
     void updateDown();
     void lookAround();
+    int getStepsAlive();
+    void setStepsAlive(int a);
 private:
     int positionRow;
     int positionColumn;
@@ -41,6 +44,7 @@ private:
     char right;
     char up;
     char down;
+    int stepsAlive;
 };
 
 
@@ -48,6 +52,7 @@ organism :: organism(int row, int column){
     positionRow = row;
     positionColumn = column;
     isAlive = true;
+    stepsAlive=0;
     if (positionRow == 0)
         up = '9';
     else
@@ -151,6 +156,13 @@ void organism::lookAround(){
     updateUp();
 }
 
+int organism::getStepsAlive() {
+    return this->stepsAlive;
+}
+
+void organism::setStepsAlive(int a){
+    this->stepsAlive=a;
+}
 /*//////////////////////////////////////////////////////////////////////////////////////////////////*/
 
 class ant : public organism {
@@ -158,19 +170,15 @@ public:
     ant(int row, int column);
     ant breed();
     void move();
-    int getStepsAlive();
 
 private:
-    int stepsAlive;
     char appearance;
 };
 ant::ant(int row, int column):organism(row,column){
     appearance = antsAppearance ;
-    stepsAlive =0;
+
 }
-int ant::getStepsAlive() {
-    return this->stepsAlive;
-}
+
 
 void ant:: move(){
     srand(time(0));
@@ -183,7 +191,7 @@ void ant:: move(){
         cout<<"killed ant on row "<<getRow()<<" and column "<<getColumn()<<endl;
     }
     else{
-        this->stepsAlive = this->stepsAlive +1;
+        this->setStepsAlive(this->getStepsAlive() +1) ;
         if (getRight() == emptySpaceAppearance)
             whatsAvailable.push_back('r');
         if (getLeft() == emptySpaceAppearance)
@@ -198,25 +206,25 @@ void ant:: move(){
             decision = whatsAvailable[randomizing];
         }
     }
-        if (decision== 'r' || decision== 'd' || decision== 'u' || decision== 'l' ){
-            doodleWorld[getRow()][getColumn()] = emptySpaceAppearance;
-            if (decision == 'u' && doodleWorld[getRow()-1][getColumn()]!=antsAppearance) {
-                this->setRow(getRow() - 1);
-                doodleWorld[getRow()][getColumn()] = antsAppearance;
-            }
-            if (decision == 'd' && doodleWorld[getRow()+1][getColumn()]!=antsAppearance) {
-                this->setRow(getRow() + 1);
-                doodleWorld[getRow()][getColumn()] = antsAppearance;
-            }
-            if (decision == 'l' && doodleWorld[getRow()][getColumn()-1]!=antsAppearance) {
-                this->setColumn(getColumn() - 1);
-                doodleWorld[getRow()][getColumn()] = antsAppearance;
-            }
-            if (decision == 'r' && doodleWorld[getRow()][getColumn()+1]!=antsAppearance) {
-                this->setColumn(getColumn() + 1);
-                doodleWorld[getRow()][getColumn()] = antsAppearance;
-            }
+    if (decision== 'r' || decision== 'd' || decision== 'u' || decision== 'l' ){
+        doodleWorld[getRow()][getColumn()] = emptySpaceAppearance;
+        if (decision == 'u' && doodleWorld[getRow()-1][getColumn()]!=antsAppearance) {
+            this->setRow(getRow() - 1);
+            doodleWorld[getRow()][getColumn()] = antsAppearance;
         }
+        if (decision == 'd' && doodleWorld[getRow()+1][getColumn()]!=antsAppearance) {
+            this->setRow(getRow() + 1);
+            doodleWorld[getRow()][getColumn()] = antsAppearance;
+        }
+        if (decision == 'l' && doodleWorld[getRow()][getColumn()-1]!=antsAppearance) {
+            this->setColumn(getColumn() - 1);
+            doodleWorld[getRow()][getColumn()] = antsAppearance;
+        }
+        if (decision == 'r' && doodleWorld[getRow()][getColumn()+1]!=antsAppearance) {
+            this->setColumn(getColumn() + 1);
+            doodleWorld[getRow()][getColumn()] = antsAppearance;
+        }
+    }
 
 
 
@@ -266,7 +274,7 @@ ant ant::breed(){
 class doodleBug : public organism{
 public:
     doodleBug(int row, int column);
-    void breed();
+    doodleBug breed();
     void move();
     int getStarving() const;
     void setStarving(int a);
@@ -279,7 +287,44 @@ doodleBug::doodleBug(int row, int column):organism(row,column){
     appearance = doodleBugsAppearance;
     stepsStarving =0;
 }
-void doodleBug:: breed(){
+doodleBug doodleBug:: breed(){
+    this->lookAround();
+    vector<char> whereToBreed;
+    doodleBug newBug(this->getRow(),this->getColumn());
+    char decision='n';
+    int randomizing;
+    if (getRight() == emptySpaceAppearance)
+        whereToBreed.push_back('r');
+    if (getLeft() == emptySpaceAppearance)
+        whereToBreed.push_back('l');
+    if (getUp() == emptySpaceAppearance)
+        whereToBreed.push_back('u');
+    if (getDown() == emptySpaceAppearance)
+        whereToBreed.push_back('d');
+
+    if (!(whereToBreed.empty())) {
+        randomizing = int(rand() % whereToBreed.size());
+        decision = whereToBreed[randomizing];
+
+    }
+
+    if (decision == 'l'){
+        newBug.setColumn(this->getColumn()-1);
+    }
+    if (decision == 'r'){
+        newBug.setColumn(this->getColumn()+1);
+    }
+
+    if (decision == 'u'){
+        newBug.setRow(this->getRow()-1);
+    }
+
+    if (decision == 'd'){
+        newBug.setColumn(this->getRow()+1);
+    }
+
+    doodleWorld[newBug.getRow()][newBug.getColumn()] = doodleBugsAppearance;
+    return newBug;
 
 }
 
@@ -296,6 +341,7 @@ void doodleBug::move(){
     char decision='n';
     int randomizing;
     this->lookAround();
+    this->setStepsAlive(getStepsAlive()+1);
     if (getRight()==antsAppearance) {
         decision = 'r';
         starving = false;
@@ -348,8 +394,10 @@ void doodleBug::move(){
             doodleWorld[getRow()][getColumn()] = doodleBugsAppearance;
         }
     }
-    if (starving)
-        this->setStarving(getStarving()+1);
+    if (starving) {
+        this->setStarving(getStarving() + 1);
+    }
+
     if (this-> getStarving() >= doodleBugStepsToStarvation) {
         kill();
         doodleWorld[this->getRow()][this->getColumn()] = emptySpaceAppearance;
@@ -363,7 +411,7 @@ void displayWorld(char arr[worldSize][worldSize]);
 int main(){
     srand(time(0));
     char next='y';
-    int tempRow=0, tempColumn=0, sizeCheck;
+    int tempRow=0, tempColumn=0, tempAntsArrSize, tempBugsArrSize, sizeCheck;
     vector<ant> antsArray, tempAntsArray;
     vector<doodleBug> doodleBugsArray, tempDoodleBugArray;
 
@@ -406,25 +454,36 @@ int main(){
             doodleBugsArray[i].move();
             doodleBugsArray[i].lookAround();
         }
+        //Bugs breeding
+        tempBugsArrSize = doodleBugsArray.size();
+        for (int i=0; i<tempBugsArrSize;i++) {
+            cout<<"Bug "<<i<<" steps alive are "<<doodleBugsArray[i].getStepsAlive()<<endl;
+            if (doodleBugsArray[i].getStepsAlive() % bugStepsToBreed == 0) {
+                doodleBugsArray.push_back(doodleBugsArray[i].breed());
+                cout << "Bug " << i << " is breeding!" << endl;
+            }
+        }
+
         //Ants taking a step
         for (int i=0; i<antsArray.size();i++) {
             antsArray[i].move();
             antsArray[i].lookAround();
         }
-        /*/ants breed
-        for (int i=0; i<antsArray.size();i++) {
+        //ants breeding
+        tempAntsArrSize = antsArray.size();
+        for (int i=0; i<tempAntsArrSize;i++) {
+            cout<<"Ant "<<i<<" steps alive are "<<antsArray[i].getStepsAlive()<<endl;
             if (antsArray[i].getStepsAlive()% antStepsToBreed == 0)
                 antsArray.push_back(antsArray[i].breed());
-        }*/
+        }
         // updating vector of surviving and new ants
         for (int i=0; i<antsArray.size();i++){
             if (antsArray[i].lifeCheck())
                 tempAntsArray.push_back(antsArray[i]);
         }
-        sizeCheck = antsArray.size() - tempAntsArray.size();
+
         antsArray=tempAntsArray;
         tempAntsArray.clear();
-        cout<<" erased "<<sizeCheck<<" ants."<<endl;
 
         // updating vector of surviving and new bugs
         for (int i=0; i<doodleBugsArray.size();i++){
